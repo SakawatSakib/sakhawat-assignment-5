534a204cf3b8fe053ced773d7d6aa8999141f141
SakawatSakib <sakawatsakib6@gmail.com>
2025-03-02 05:48:40 +0600

    btn bg random color added

--- script/devBoard.js
+++ script/devBoard.js
@@ -10,8 +10,27 @@ document.getElementById('date').innerHTML =`<span class='font-medium'>${formatte
 
 // task header part current date set  End here
 
-// discover toggle task section to blog section Start Here
+// discover toggle task dev board section to blog section Start Here
+ document.getElementById('discover-box').addEventListener('click',function (){
+    window.location.href ='blogs.html';
+ })
+ 
+// discover toggle task dev board section to blog section End Here
 
 
-// discover toggle task section to blog section End Here
+// change the background color randomly after bg button clicked Start Here
+
+const bgButton = document.getElementById('bg-btn');
+bgButton.addEventListener('click',function(){
+    let r = Math.floor(Math.random()*255);
+    let g = Math.floor(Math.random()*255);
+    let b = Math.floor(Math.random()*255);
+    let o = Math.random()* 9.71;
+    let alpha = o.toFixed(1);
+    let randomColor = `rgb(${r},${g},${b},${alpha})`
+    document.body.style.backgroundColor = randomColor;
+})
+
+// change the background color randomly after bg button clicked End Here
+
 
