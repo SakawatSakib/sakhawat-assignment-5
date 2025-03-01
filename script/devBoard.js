28dc27ce14befd04e027194094edd15c3f68a8d7
SakawatSakib <sakawatsakib6@gmail.com>
2025-03-02 04:19:28 +0600

    date time dynamically added

--- script/devBoard.js
+++ script/devBoard.js
@@ -1,6 +1,17 @@
-// task header part current date set 
+// task header part current date set  start here
 
 let date = new Date().toDateString().split(' ');
 date[0] += ' ,';
+
+
 let formattedDate = date.join(' ');
-document.getElementById('date').innerText = formattedDate;
+
+document.getElementById('date').innerHTML =`<span class='font-medium'>${formattedDate.slice(0,5)}</span> <br> ${formattedDate.slice(5,17)}`
+
+// task header part current date set  End here
+
+// discover toggle task section to blog section Start Here
+
+
+// discover toggle task section to blog section End Here
+
