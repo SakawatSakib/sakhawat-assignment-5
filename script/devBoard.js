083a241737c6693a32013798744d251affed4d05
SakawatSakib <sakawatsakib6@gmail.com>
2025-03-01 18:07:17 +0600

    tasks header added

---
+++ script/devBoard.js
@@ -0,0 +1,6 @@
+// task header part current date set 
+// 
+let date = new Date().toDateString().split(' ');
+date[0] += ' ,';
+let formattedDate = date.join(' ');
+document.getElementById('date').innerText = formattedDate;
